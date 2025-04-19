0b796b13cc5892d37e6b346f1e82ad769c866453
Pavel Zharkov <zharkov.p@dornet.ru>
2025-04-19 12:08:29 +0300

    init v2

---
+++ src/components/admin/AdminComponent.jsx
@@ -0,0 +1,10 @@
+function AdminComponent() {
+    return (
+        <div>
+            <h2>Административная страница</h2>
+            <p>Это содержимое административной части.</p>
+        </div>
+    )
+}
+
+export default AdminComponent
---
+++ src/components/client/ClientComponent.jsx
@@ -0,0 +1,10 @@
+function ClientComponent() {
+    return (
+        <div>
+            <h2>Клиентсккая страница</h2>
+            <p>Это содержимое клиентской части.</p>
+        </div>
+    )
+}
+
+export default ClientComponent
